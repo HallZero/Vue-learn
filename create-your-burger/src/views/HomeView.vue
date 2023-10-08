664314fd21b93c9e3332bdf67adf145e02d63af1
HallZero <filipikikuchi@gmail.com>
2023-10-08 16:52:59 -0300

    feat(components): :sparkles: Add components

--- create-your-burger/src/views/HomeView.vue
+++ create-your-burger/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <p>formulario</p>
   </div>
 </template>
 
 <script>
-
+  import Banner from '../components/Banner.vue';
+  export default {
+    name: 'Home',
+    components: {
+      Banner
+    }
+  }
 </script>
